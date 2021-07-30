0a6850c87848985ba416da7031f1debf40af9212
Yefenny Chireno <yefenny300@icloud.com>
2021-07-30 11:13:21 -0400

    updated exercise_04 and ecercise_05

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,7 +16,26 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        byte a = 120;
+        System.out.println("byte a is: " + a);
 
+        char b = 'a';
+        System.out.println("char b is:" + b);
+
+        short c = 12344;
+        System.out.println("short c is: "+c);
+
+        long d = 1232333222333l;
+        System.out.println("Long d is: "+ d);
+
+        float e = 1212.2323324f;
+        System.out.println("float e is: " + e);
+
+        double f = 11223.2323232;
+        System.out.println("double f is:" + f);
+
+        boolean g = true;
+        System.out.println("boolean g is:"+ g);
     }
 
 }
